a79c59820e786652fa632182fe95720081790791
Marco Speranza <marco.speranza79@gmail.com>
2021-12-08 16:02:33 +0100

    Add mock store

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {create} from "react-test-renderer";
+import NewActivity from "./components/NewActivity";
+import {Provider} from "react-redux";
+import configureStore from 'redux-mock-store' //ES6 modules
 
-xtest('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mockStore = configureStore([])
+
+it('app should render without throwing an error', function() {
+  const store = mockStore();
+
+  const container = create(
+      <Provider store={store}>
+        <App />
+      </Provider>);
+  expect(container).not.toBeNull();
 });
