efd663eea8b9467ce35490de4367ff6fd73b8693
Marco Speranza <marco.speranza79@gmail.com>
2021-12-13 23:26:04 +0100

    Add signout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Button} from "primereact/button";
+import {LoginControllerService} from "../generated/api";
 
 
 /**
@@ -21,16 +22,29 @@ export class Header extends React.Component<Props, HeaderState> {
         }
     }
 
+    /**
+     * quick and dirty implementation of logut.
+     */
+    logout = () => {
+        LoginControllerService.logout()
+            .then((value: string) => {})
+            .catch(reason => {window.location.reload()})
+    }
+
     render() {
         return (
             <div className="header flex flex-row justify-content-between">
-                <span className={'font-bold text-xl ml-2 my-auto title'}>Task Management System</span>
+                <span className={'font-bold ml-2 my-auto title text-sm sm:text-xl'}>Task Management System</span>
 
                 <div className={'flex flex-row login my-auto'}>
                     <span className={'my-auto'}>{this.props.username}</span>
                     <Button icon="pi pi-user"
                             disabled={ this.props.username !== '' && this.props.username !== 'guest'}
                             className="p-button-rounded p-button-info p-button-text login"  onClick={this.props.login}/>
+                    {this.props.username !== 'guest' &&
+                        <Button icon="pi pi-sign-out"
+                             className="p-button-rounded p-button-info p-button-text login" onClick={this.logout}/>
+                    }
                 </div>
             </div>);
     }
