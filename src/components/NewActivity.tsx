8df7fdb44aefc9ddf897ece17706cd2538cfcfb7
Marco Speranza <marco.speranza79@gmail.com>
2021-12-13 22:09:06 +0100

    add accordion

--- src/components/NewActivity.tsx
+++ src/components/NewActivity.tsx
@@ -9,6 +9,7 @@ import {InputMask} from "primereact/inputmask";
 import {Calendar} from "primereact/calendar";
 import {Dialog} from "primereact/dialog";
 import {format} from "date-fns";
+import {Accordion, AccordionTab} from "primereact/accordion";
 
 
 /**
@@ -192,72 +193,64 @@ export class NewActivity extends React.Component<Props, NewActivityState> {
     render() {
         return (
             <div className="flex flex-row justify-content-end flex-wrap mt-6">
-                <Toast ref={this.toastBL} position="bottom-left" />
 
-                { !this.state.showNewDialogue &&
-                <Button id="open-new-activity-dialogue"
-                        label="New"
-                        className="w-full p-button-sm mb-2 p-button-outlined p-button-secondary"
-                        onClick={this.toggle}/>
-                }
+                <Accordion id={'open-new-activity-dialogue'} activeIndex={0} className="w-full">
+                    <AccordionTab  header="Add Task">
+                        <div id={'newActivityDialogue'}>
+                            <div className="mt-4">
+                                <label htmlFor="description">Description</label>
+                                <InputText className="w-full" id="description"
+                                           value={this.state.description}
+                                           onChange={(e) => this.setDescription(e.target.value)}/>
+                            </div>
+                            <div className="mt-4">
+                                <label htmlFor="date">Date</label>
 
-                {
-                    this.state.showNewDialogue &&
-                    <div id="newActivityDialogue" className="shadow-2 p-2 border-round w-full">
-                        <div className={'flex flex-nowrap justify-content-between'}>
-                            <h3 className="float-right">Add Task</h3>
-                            <Button icon="pi pi-angle-up"
-                                    className="p-button-rounded p-button-text my-auto"
-                                    onClick={this.toggle}
-                            />
-                        </div>
-                        <div className="mt-4">
-                            <label htmlFor="description">Description</label>
-                            <InputText className="w-full" id="description"
-                                       value={this.state.description}
-                                       onChange={(e) => this.setDescription(e.target.value)}/>
-                        </div>
-                        <div className="mt-4">
-                            <label htmlFor="date">Date</label>
+                                {this.renderCalendar(true)}
 
-                            {this.renderCalendar(true)}
+                                <div className={'flex flex-row flex-nowrap md:hide'}>
+                                    <InputMask id="date"
+                                               mask="9999-99-99"
+                                               slotChar="yyyy-mm-dd"
+                                               value={this.state.date}
+                                               className={classNames({
+                                                   'w-full': true,
+                                                   'p-invalid': this.state.error,
+                                                   'no-round-border-right': true
+                                               })}
+                                               onChange={(e) => this.setDate(e.value)}/>
+                                    <Button id={'calendar-btn'}
+                                            className={'no-round-border-left'}
+                                            icon="pi pi-calendar"
+                                            onClick={() => this.setCalendarVisibility(true)}/>
+                                    <Dialog visible={this.state.showCal}
+                                            maximizable
+                                            modal
+                                            style={{width: '50vw'}}
+                                            footer={this.renderFooter()}
+                                            onHide={() => this.setCalendarVisibility(false)}>
 
-                            <div className={'flex flex-row flex-nowrap md:hide'}>
-                                <InputMask id="date"
-                                           mask="9999-99-99"
-                                           slotChar="yyyy-mm-dd"
-                                           value={this.state.date}
-                                           className={classNames({
-                                               'w-full': true,
-                                               'p-invalid': this.state.error,
-                                               'no-round-border-right': true
-                                           })}
-                                           onChange={(e) => this.setDate(e.value)}/>
-                                <Button  className={'no-round-border-left'}
-                                         icon="pi pi-calendar"
-                                         onClick={() => this.setCalendarVisibility(true)}/>
-                                <Dialog visible={this.state.showCal}
-                                        maximizable
-                                        modal
-                                        style={{width: '50vw'}}
-                                        footer={this.renderFooter()}
-                                        onHide={() => this.setCalendarVisibility(false)}>
+                                        {this.renderCalendar(false)}
+                                    </Dialog>
+                                </div>
 
-                                    {this.renderCalendar(false)}
-                                </Dialog>
                             </div>
 
-                        </div>
+                            <Button label="Save"
+                                    id={'save-activity'}
+                                    loading={this.props.progress}
+                                    disabled={!(!!this.state.description && !!this.state.date && !this.state.error)}
+                                    className="w-full p-button-sm mt-4 p-button-outlined p-button-primary"
+                                    onClick={this.addNewActivity}/></div>
 
-                        <Button label="Save"
-                                id={'save-activity'}
-                                loading={this.props.progress}
-                                disabled={!(!!this.state.description && !!this.state.date && !this.state.error)}
-                                className="w-full p-button-sm mt-4 p-button-outlined p-button-primary"
-                                onClick={this.addNewActivity}/>
+                    </AccordionTab>
+                </Accordion>
+                <Toast ref={this.toastBL} position="bottom-left" />
 
-                    </div>
-                }
+
+
+
+
             </div>);
     }
 
